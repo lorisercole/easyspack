611fcce3e52eb123655cdd6915a173bc5b8ca96d
Loris Ercole <loris.ercole@gmail.com>
2026-02-09 16:01:57 +0100

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = readme_file.read_text() if readme_file.exists() else ""
 
 setup(
     name="spood",
-    version="0.1.0",
+    version="0.2.0",
     description="A bridge between EESSI/EasyBuild and Spack",
     long_description=long_description,
     long_description_content_type="text/markdown",
